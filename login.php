d9d00562088b89b7a6c1ebd78b202ca1979595f2
BenNordmann <88218941+BenNordmann@users.noreply.github.com>
2023-05-01 20:08:40 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,18 +1,13 @@
 
 	<?php
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			// Retrieve form data
 			$username = $_POST['username'];
 			$password = $_POST['password'];
-			
-			// Connect to MySQL database
 			$servername = 'studentdb-maria.gl.umbc.edu';
 			$dbusername = 'nordman1';
 			$dbpassword = 'nordman1';
 			$dbname = 'nordman1';
 			$con = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
-
-			// Verify credentials
 			$query = "SELECT * FROM Users WHERE username='$username' AND password='$password'";
 			$result = mysqli_query($con, $query);
 			if (mysqli_num_rows($result) == 1) {
@@ -24,8 +19,6 @@
 			} else {
 				echo '<p>Login failed. Please check your username and password and try again.</p>';
 			}
-
-			// Close database connection
 			mysqli_close($con);
 		}
 	?>
